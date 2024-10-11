3d1ed06f65a440075d059707b9f8080eac2f7c88
COLE <2020clicks@gmail.com>
2024-10-11 19:49:52 +0100

    -Open BUg, gcc wont link #includes properly

---
+++ Pi5/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace cv;
+int main()
+{
+
+    std::string image_path = "/home/5team4/Documents/RTCP/RTCP_Image_Quality/cat.jpg";
+    Mat img = imread(image_path, IMREAD_COLOR);
+
+    imshow("Display window", img);
+    int k = waitKey(0); // Wait for a keystroke in the window
+    return 0;
+}
