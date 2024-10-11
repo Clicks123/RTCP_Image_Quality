80e28f592ebfdc8b8a2aea26e875d9f77566d8c8
Clicks123 <2020clicks@gmail.com>
2024-10-11 12:12:40 -0500

    -idk

--- CameraTest.py
+++ CameraTest.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(1, cv.CAP_DSHOW)
+cap = cv.VideoCapture(0, cv.CAP_DSHOW)
 cap1 = cv.VideoCapture(0, cv.CAP_DSHOW)
 if not cap.isOpened():
  print("Cannot open camera")
@@ -21,12 +21,13 @@ while True:
     print("Can't receive frame1 (stream end?). Exiting ...")
     break
  # Our operations on the frame come here
- gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+ gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #this will convert frame to gray scale
  # Display the resulting frame
- cv.imshow('frame', frame)
+ cv.imshow('frame', gray)
  cv.imshow('frame1', frame1)
  if cv.waitKey(1) == ord('q'):
     break
+
 # When everything done, release the capture
 cap.release()
 cap1.release()
