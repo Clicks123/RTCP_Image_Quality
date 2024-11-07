d0383ee37535be8d57440bc585a3d95bb54a8f3f
cole <2020clicks@gmail.com>
2024-11-07 16:31:24 +0000

    Fixed CMake

--- ProcessorSubsystemOperationTest/cmake_install.cmake
+++ ProcessorSubsystemOperationTest/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/5team4/Documents/RTCP/RTCP_Image_Quality/ProcessorSubsystemTest
+# Install script for directory: /home/5team4/Documents/RTCP/RTCP_Image_Quality/ProcessorSubsystemOperationTest
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/5team4/Documents/RTCP/RTCP_Image_Quality/ProcessorSubsystemTest/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/5team4/Documents/RTCP/RTCP_Image_Quality/ProcessorSubsystemOperationTest/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
