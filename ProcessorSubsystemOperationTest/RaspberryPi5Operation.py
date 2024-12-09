e0b6e4bdef61de0a57bdc6d3d61130d73e3c17fc
Primus <primusm@msoe.edu>
2024-12-09 15:19:43 -0600

    Changing processor subsystem Python scripts to just turn on a LED

--- ProcessorSubsystemOperationTest/RaspberryPi5Operation.py
+++ ProcessorSubsystemOperationTest/RaspberryPi5Operation.py
@@ -1,20 +1,4 @@
 from gpiozero import LED
-import serial
-import time
 
 led=LED(24)
 led.on()
-
-ser = serial.Serial(
-        port='/dev/serial0',
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-)
-counter=0
-while True:
-        ser.write('Hello from Pi 5: %d\n'%(counter))
-        time.sleep(1)
-        counter+=1
